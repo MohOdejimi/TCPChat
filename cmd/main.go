b1e113dad2479685ea8a0606cd69ccd5491e6f3c
MohOdejimi <mohammedodejimi@gmail.com>
2026-05-09 16:49:33 +0100

    Implemented Registry Struct to store and contain client info, Built the logic to enable client communicate and broadcast their messages utiliziing goroutines and channels as message pipelines

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-
+	"log"
 
 	"github.com/MohOdejimi/TCPChat/internal/cli"
+	"github.com/MohOdejimi/TCPChat/internal/hub"
 	"github.com/MohOdejimi/TCPChat/internal/server"
 )
 
 func main() {
 	port, maxConnections := cli.Flags()
-	server.TCPServer(port, maxConnections)
-}	
+
+	chatHub := hub.NewHub(server.Registry)
+	go chatHub.Run()
+	err := server.TCPServer(port, maxConnections, chatHub)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	select {}
+
+}
